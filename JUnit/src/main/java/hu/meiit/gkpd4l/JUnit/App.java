b882e562b92e2c818069200f3a185d1b5bdfdf50
Pohobicsrobi <pohobicsrobi@gmail.com>
2020-12-02 18:53:21 +0100

    Feleves feladat

---
+++ JUnit/src/main/java/hu/meiit/gkpd4l/JUnit/App.java
@@ -0,0 +1,13 @@
+package hu.meiit.gkpd4l.JUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JUnit/src/main/java/hu/meiit/gkpd4l/JUnit/Beadando.java
@@ -0,0 +1,12 @@
+package hu.meiit.gkpd4l.JUnit;
+
+public class Beadando {
+
+	public double pitagorasz(double a, double b) {
+		
+		return Math.sqrt(a*a+b*b);
+		
+		//return 0;
+		
+	}
+}
